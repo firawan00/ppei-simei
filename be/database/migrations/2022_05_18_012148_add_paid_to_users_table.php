e5cf68d330e98ffb094a4abb8ab13738e62dd294
geneshairzan <genesha.irzan@gmail.com>
2022-05-29 19:03:43 +0700

    gip

--- be/database/migrations/2022_05_18_012148_add_paid_to_users_table.php
+++ be/database/migrations/2022_05_18_012148_add_paid_to_users_table.php
@@ -13,26 +13,14 @@ class AddPaidToUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('status', function (Blueprint $table) {
-            //
-            $table->id();
-            $table->string('name')->nullable();
-        });
-
-        Schema::create('user_role', function (Blueprint $table) {
-            //
-            $table->id();
-            $table->string('name')->nullable();
-        });
-
 
         Schema::table('users', function (Blueprint $table) {
             //
             $table->string('name')->nullable()->change();
             $table->string('password')->nullable()->change();
 
-            // $table->foreignId('status_id')->after('id')->default(1)->constrained('status')->cascadeOnDelete();
-            // $table->foreignId('role_id')->after('id')->default(1)->constrained('user_role')->cascadeOnDelete();
+            $table->foreignId('status_id')->after('id')->default(1)->constrained('status')->cascadeOnDelete();
+            $table->foreignId('role_id')->after('id')->default(1)->constrained('user_role')->cascadeOnDelete();
 
             $table->boolean('gender')->nullable()->after('password');
             $table->string('img_path')->nullable()->after('password');
@@ -43,35 +31,6 @@ class AddPaidToUsersTable extends Migration
             $table->text('about')->nullable()->after('password');;
 
             $table->softDeletes();
-        });
-
-        Schema::create('users_store', function (Blueprint $table) {
-            //
-            $table->uuid('id')->primary();
-            $table->uuid('user_id');
-            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
-
-            $table->string('name')->nullable();
-            $table->text('about')->nullable();
-
-            $table->string('phone')->nullable();
-            $table->string('address')->nullable();
-            $table->string('website')->nullable();
-            $table->string('instagram')->nullable();
-            $table->string('facebook')->nullable();
-
-            $table->text('opportunities')->nullable();
-        });
-
-        Schema::create('users_store_product', function (Blueprint $table) {
-            //
-            $table->uuid('id')->primary();;
-            $table->uuid('store_id');
-            $table->foreign('store_id')->references('id')->on('users_store')->onDelete('cascade');
-
-            $table->string('name')->nullable();
-            $table->text('desc')->nullable();
-            $table->integer('price')->nullable();
         });
     }
 
---
+++ be/database/migrations/2022_05_19_005529_user_store.php
@@ -0,0 +1,56 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class UserStore extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        Schema::create('users_store', function (Blueprint $table) {
+            //
+            $table->uuid('id')->primary();
+            $table->uuid('user_id');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+
+            $table->string('name')->nullable();
+            $table->text('about')->nullable();
+
+            $table->string('phone')->nullable();
+            $table->string('address')->nullable();
+            $table->string('website')->nullable();
+            $table->string('instagram')->nullable();
+            $table->string('facebook')->nullable();
+
+            $table->text('opportunities')->nullable();
+        });
+
+        Schema::create('users_store_product', function (Blueprint $table) {
+            //
+            $table->uuid('id')->primary();;
+            $table->uuid('store_id');
+            $table->foreign('store_id')->references('id')->on('users_store')->onDelete('cascade');
+
+            $table->string('name')->nullable();
+            $table->text('desc')->nullable();
+            $table->integer('price')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
