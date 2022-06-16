976ff176c9c49b1faead9c6e78b8dd094603690a
geneshairzan <genesha.irzan@gmail.com>
2022-06-16 20:27:05 +0700

    gip

---
+++ fe/dist/app.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700&display=swap"
+      rel="stylesheet"
+    />
+    <link rel="manifest" href="/public/site.webmanifest" />
+    <title>Genesha DEV</title>
+    <script type="module" crossorigin src="/public/assets/index.4def92bd.js"></script>
+    <link rel="stylesheet" href="/public/assets/index.dfab9494.css">
+  </head>
+  <body>
+    <div id="root"></div>
+    
+  </body>
+</html>
