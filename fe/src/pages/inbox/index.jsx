c5638c0aa182b70376704a1e3e27a3e65f4330c4
genesha <genesha.irzan@gmail.com>
2023-07-26 17:28:50 +0700

    gip rebuild

--- fe/src/pages/inbox/index.jsx
+++ fe/src/pages/inbox/index.jsx
@@ -8,6 +8,7 @@ import { fdate } from "@/component/helper/formating";
 export default function App(props) {
   const [data, setdata] = useState([]);
   const nav = useNavigate();
+  const [showAll, setshowAll] = useState(false);
 
   const field = [
     { data: "from", label: "From", w: "20%" },
@@ -20,7 +21,7 @@ export default function App(props) {
   useEffect(() => {
     async function fetchData() {
       let res = await fetcher({
-        url: `inbox/to`,
+        url: `inbox/to?all=1`,
         method: "get",
       });
       setdata(res);
@@ -39,22 +40,29 @@ export default function App(props) {
     nav(link, true);
   }
 
+  function mailFilter(d) {
+    if (showAll) return true;
+    var today = new Date().getDate();
+    let refdate = new Date(d.created_at).getDate();
+    return refdate >= today;
+  }
+
   return (
     <Stack spacing={2}>
-      <Typography variant="h4" className="f-uppercase f-bold" color="primary">
-        INBOX
-      </Typography>
+      <Stack direction={"row"} justifyContent={"space-between"}>
+        <Typography variant="h4" className="f-uppercase f-bold" color="primary">
+          INBOX
+        </Typography>
+        <Button variant="outlined" onClick={() => setshowAll(!showAll)}>
+          {!showAll ? "All " : "Today"}
+        </Button>
+      </Stack>
+
       <Stack>
         <Divider />
         <Stack direction="row" my={1}>
           {field.map((d) => (
-            <Typography
-              key={d.label}
-              width={d.w}
-              variant="subtitle1"
-              className="f-uppercase f-bold"
-              color="primary"
-            >
+            <Typography key={d.label} width={d.w} variant="subtitle1" className="f-uppercase f-bold" color="primary">
               {d.label}
             </Typography>
           ))}
@@ -62,12 +70,8 @@ export default function App(props) {
         <Divider />
       </Stack>
       {data.length > 0 &&
-        data.map((d) => (
-          <Stack
-            key={d.id}
-            direction="row"
-            fontWeight={d.hasread ? "" : "bold"}
-          >
+        data.filter(mailFilter).map((d) => (
+          <Stack key={d.id} direction="row" fontWeight={d.hasread ? "" : "bold"}>
             <Stack width={field[0].w}>{d.from.name}</Stack>
             <Stack width={field[1].w}>{d.title}</Stack>
             <Stack width={field[2].w}>{d.ref_id}</Stack>
--- fe/src/pages/outbox/index.jsx
+++ fe/src/pages/outbox/index.jsx
@@ -1,12 +1,13 @@
 import React, { useState, useEffect } from "react";
 
-import { Box, Stack, Divider, Typography } from "@mui/material";
+import { Box, Stack, Divider, Typography, Button } from "@mui/material";
 import { fetcher } from "@component/gip-useForm/fetcher";
 import { Link } from "react-router-dom";
 import { fdate } from "@/component/helper/formating";
 
 export default function App(props) {
   const [data, setdata] = useState([]);
+  const [showAll, setshowAll] = useState(false);
 
   const field = [
     { data: "to", label: "To", w: "20%" },
@@ -27,22 +28,29 @@ export default function App(props) {
     fetchData();
   }, []);
 
+  function mailFilter(d) {
+    if (showAll) return true;
+    var today = new Date().getDate();
+    let refdate = new Date(d.created_at).getDate();
+    return refdate >= today;
+  }
+
   return (
     <Stack spacing={2}>
-      <Typography variant="h4" className="f-uppercase f-bold" color="primary">
-        Outbox
-      </Typography>
+      <Stack direction={"row"} justifyContent={"space-between"}>
+        <Typography variant="h4" className="f-uppercase f-bold" color="primary">
+          Outbox
+        </Typography>
+        <Button variant="outlined" onClick={() => setshowAll(!showAll)}>
+          {!showAll ? "All " : "Today"}
+        </Button>
+      </Stack>
+
       <Stack>
         <Divider />
         <Stack direction="row" my={1}>
           {field.map((d, ix) => (
-            <Typography
-              key={d.label}
-              width={d.w}
-              variant="subtitle1"
-              className="f-uppercase f-bold"
-              color="primary"
-            >
+            <Typography key={d.label} width={d.w} variant="subtitle1" className="f-uppercase f-bold" color="primary">
               {d.label}
             </Typography>
           ))}
@@ -50,7 +58,7 @@ export default function App(props) {
         <Divider />
       </Stack>
       {data.length > 0 &&
-        data.map((d) => (
+        data.filter(mailFilter).map((d) => (
           <Stack key={d.id} direction="row">
             {/* <Stack width={field[0].w}>{d.from.name}</Stack> */}
             <Stack width={field[0].w}>{d.to.name}</Stack>
